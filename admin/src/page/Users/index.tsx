2929d6a075891cf8082866c743e25e4745178bcb
Miguel Barrera <miguel.barrera.205m@gmail.com>
2024-05-14 07:23:32 -0500

    Creacion de navbar, page y navigation de usuarios

---
+++ admin/src/page/Users/index.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Users = () => {
+  return <Conatiner>Users</Conatiner>;
+};
+
+const Conatiner = styled.div`
+  margin-top: 20px;
+  margin-left: 15%;
+  width: 85%;
+  height: 100vh;
+  padding-left: 200px;
+  padding-right: 200px;
+  overflow-y: auto;
+`;
