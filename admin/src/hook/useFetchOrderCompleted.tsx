4ddc70104d7d0693718df27ba7b842a0fec01eca
James Orozco <jamesorozcoh@gmail.com>
2024-05-14 14:29:10 -0500

    working on useFecthOrderComplete

--- admin/src/hook/useFetchOrderCompleted.tsx
+++ admin/src/hook/useFetchOrderCompleted.tsx
@@ -1,20 +1,26 @@
 import { useEffect, useState } from "react";
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "../firebase/config";
-
+import { OrderCompleted } from "../interfaces/OrderCompleted";
 
 const useFetchOrderCompleted = () => {
-  const [order, setOrder] = useState<unknown | null>(null);
+  const [order, setOrder] = useState<OrderCompleted | null>(null);
 
   useEffect(() => {
     const fetchOrder = async () => {
       try {
         const ordersRef = collection(db, "orderCompleted");
         const orderSnapshot = await getDocs(ordersRef);
+
         orderSnapshot.forEach((doc) => {
           const orderData = {
             orderId: doc.data().orderId,
-            order: doc.data().order || ""
+            order: doc.data().order || "",
+            createdAt: doc.data().order.createdAt,
+            state: doc.data().order.state,
+            name: doc.data().order.name,
+            paymentMethod: doc.data().order.products.paymentMethod,
+            OpcionDeEntrega: doc.data().order.products.OpcionDeEntrega,
           };
           setOrder(orderData);
         });
